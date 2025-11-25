2cc36c4f88d64556ae11258197dfbb885cf0ff40
alexandreribeiro0026385333-source <alexandreribeiro0026385333@gmail.com>
2025-11-25 15:40:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,30 @@ const pool = new Pool({
 // --- ROTAS DE API ---
 
 // Cadastro Motora
+
+
+app.post('/motora', async (req, res) => {
+  const { nome, telefone, senha, email, placa } = req.body;
+  const query = `
+    INSERT INTO motora 
+    (id, nome, telefone, senha, latitude, longitude, status, placa, 
+     total_corridas_dia, total_valor_dia, passageiro_id, corrida_id, created_at, email) 
+    VALUES (DEFAULT, $1, $2, $3, NULL, NULL, $4, $5, 0, 0, NULL, NULL, NOW(), $6)
+  `;
+  try {
+    await pool.query(query, [nome, telefone, senha, 'offline', placa, email]);
+    res.json({ ok: true });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+
+
+
+/"
 app.post('/motora', async (req, res) => {
   const { nome, telefone, senha, email, placa } = req.body;
   const query = 'INSERT INTO motora (nome, telefone, senha, email, placa, status) VALUES ($1, $2, $3, $4, $5, $6)';
@@ -29,7 +53,7 @@ app.post('/motora', async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-});
+});    */
 
 // Cadastro Boneco
 app.post('/boneco', async (req, res) => {
